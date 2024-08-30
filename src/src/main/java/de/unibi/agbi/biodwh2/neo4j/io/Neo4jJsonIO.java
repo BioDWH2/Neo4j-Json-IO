16ff333a9734ab469d5d61431edf98b39cd9c7d2
Marcel Friedrichs <mfriedrichs@techfak.uni-bielefeld.de>
2024-08-30 10:32:10 +0200

    Optimize performance

--- src/src/main/java/de/unibi/agbi/biodwh2/neo4j/io/Neo4jJsonIO.java
+++ src/src/main/java/de/unibi/agbi/biodwh2/neo4j/io/Neo4jJsonIO.java
@@ -50,8 +50,7 @@ public class Neo4jJsonIO {
         checkForUpdate();
         if (commandLine.help) {
             printHelp(commandLine);
-        }
-        else if (StringUtils.isNotEmpty(commandLine.exportFilePath) && StringUtils.isNotEmpty(commandLine.endpoint))
+        } else if (StringUtils.isNotEmpty(commandLine.exportFilePath) && StringUtils.isNotEmpty(commandLine.endpoint))
             export(commandLine.exportFilePath, commandLine.endpoint, commandLine.username, commandLine.password);
         else {
             LOGGER.error("export and endpoint arguments must be specified");
@@ -118,18 +117,25 @@ public class Neo4jJsonIO {
         try (final OutputStream stream = new GZIPOutputStream(Files.newOutputStream(exportFile));
              final Driver driver = GraphDatabase.driver(endpoint, getAuthToken(username, password))) {
             try (final Session session = driver.session()) {
-                final var relationshipTypes = session.run("CALL db.relationshipTypes").stream().map(
-                        l -> l.get(0).asString()).toArray(String[]::new);
+                LOGGER.info("Retrieve database statistics...");
+                final var nodeCount = session.run("MATCH (n) RETURN count(n)").next().get(0).asLong();
+                final var edgeCount = session.run("MATCH ()-[r]->() RETURN count(r)").next().get(0).asLong();
+                LOGGER.info("Exporting database with {} nodes and {} edges...", nodeCount, edgeCount);
                 final var writer = new PrintWriter(stream, true, StandardCharsets.UTF_8);
                 writer.println("{");
                 writer.println("  \"graph\": {");
                 writer.println("    \"nodes\": {");
                 boolean first = true;
                 {
-                    final var result = session.run("MATCH (n) RETURN n");
+                    LOGGER.info("Exporting nodes...");
+                    long counter = 1;
+                    final var tx = session.beginTransaction();
+                    final var result = tx.run("MATCH (n) RETURN n");
                     while (result.hasNext()) {
                         final var record = result.next();
                         final var node = record.get(0).asNode();
+                        if (counter % 10_000 == 0)
+                            LOGGER.info("\tProgress {}/{}", counter, nodeCount);
                         if (!first)
                             writer.println(",");
                         first = false;
@@ -137,30 +143,37 @@ public class Neo4jJsonIO {
                         writer.println("        \"label\": \"" + String.join(";", node.labels()) + "\",");
                         writer.println("        \"metadata\": " + createMetadata(node));
                         writer.print("      }");
+                        counter++;
                     }
+                    tx.close();
                 }
                 if (!first)
                     writer.println();
                 writer.println("    },");
                 writer.println("    \"edges\": [");
                 first = true;
-                for (final String label : relationshipTypes) {
-                    final var result = session.run("MATCH ()-[r:" + label + "]->() RETURN r");
-                    while (result.hasNext()) {
-                        final var record = result.next();
-                        final var edge = record.get(0).asRelationship();
-                        if (!first)
-                            writer.println(",");
-                        first = false;
-                        writer.println("      {");
-                        writer.println("        \"id\": \"" + edge.elementId() + "\",");
-                        writer.println("        \"source\": \"" + edge.startNodeElementId() + "\",");
-                        writer.println("        \"target\": \"" + edge.endNodeElementId() + "\",");
-                        writer.println("        \"label\": \"" + label + "\",");
-                        writer.println("        \"metadata\": " + createMetadata(edge));
-                        writer.print("      }");
-                    }
+                LOGGER.info("Exporting edges...");
+                long counter = 1;
+                final var tx = session.beginTransaction();
+                final var result = tx.run("MATCH ()-[r]->() RETURN r");
+                while (result.hasNext()) {
+                    final var record = result.next();
+                    final var edge = record.get(0).asRelationship();
+                    if (counter % 10_000 == 0)
+                        LOGGER.info("\tProgress {}/{}", counter, edgeCount);
+                    if (!first)
+                        writer.println(",");
+                    first = false;
+                    writer.println("      {");
+                    writer.println("        \"id\": \"" + edge.elementId() + "\",");
+                    writer.println("        \"source\": \"" + edge.startNodeElementId() + "\",");
+                    writer.println("        \"target\": \"" + edge.endNodeElementId() + "\",");
+                    writer.println("        \"label\": \"" + edge.type() + "\",");
+                    writer.println("        \"metadata\": " + createMetadata(edge));
+                    writer.print("      }");
+                    counter++;
                 }
+                tx.close();
                 if (!first)
                     writer.println();
                 writer.println("    ]");
