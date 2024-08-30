57f1cc816eade9180ff9a7d9a4a70210196ffefd
Marcel Friedrichs <mfriedrichs@techfak.uni-bielefeld.de>
2024-08-30 08:16:28 +0200

    First working version

--- src/src/main/java/de/unibi/agbi/biodwh2/neo4j/io/Neo4jJsonIO.java
+++ src/src/main/java/de/unibi/agbi/biodwh2/neo4j/io/Neo4jJsonIO.java
@@ -1,5 +1,6 @@
 package de.unibi.agbi.biodwh2.neo4j.io;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import de.unibi.agbi.biodwh2.neo4j.io.model.CmdArgs;
@@ -10,18 +11,25 @@ import org.apache.commons.lang3.StringUtils;
 import org.neo4j.driver.*;
 import org.apache.logging.log4j.Logger;
 import org.apache.logging.log4j.LogManager;
+import org.neo4j.driver.types.Entity;
 import picocli.CommandLine;
 
 import java.io.IOException;
+import java.io.OutputStream;
+import java.io.PrintWriter;
 import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.*;
 import java.util.jar.Manifest;
+import java.util.zip.GZIPOutputStream;
 
 public class Neo4jJsonIO {
     private static final Logger LOGGER = LogManager.getLogger(Neo4jJsonIO.class);
     private static final String RELEASE_URL = "https://api.github.com/repos/BioDWH2/Neo4j-Json-IO/releases";
+    private static final ObjectMapper MAPPER = new ObjectMapper();
 
     private Neo4jJsonIO() {
     }
@@ -52,10 +60,9 @@ public class Neo4jJsonIO {
         final Version currentVersion = getCurrentVersion();
         Version mostRecentVersion = null;
         String mostRecentDownloadUrl = null;
-        final ObjectMapper mapper = new ObjectMapper();
         try {
             final var releaseUrl = new URL(RELEASE_URL);
-            final List<GithubRelease> releases = mapper.readValue(releaseUrl, new TypeReference<>() {
+            final List<GithubRelease> releases = MAPPER.readValue(releaseUrl, new TypeReference<>() {
             });
             for (final GithubRelease release : releases) {
                 final Version version = Version.tryParse(release.tagName.replace("v", ""));
@@ -103,16 +110,75 @@ public class Neo4jJsonIO {
     }
 
     private void export(final String exportFilePath, final String endpoint, final String username,
-                               final String password) {
+                        final String password) {
         final Path exportFile = Paths.get(exportFilePath);
-        try (final Driver driver = GraphDatabase.driver(endpoint, getAuthToken(username, password))) {
+        try (final OutputStream stream = new GZIPOutputStream(Files.newOutputStream(exportFile));
+             final Driver driver = GraphDatabase.driver(endpoint, getAuthToken(username, password))) {
             try (final Session session = driver.session()) {
-                // TODO
+                final var relationshipTypes = session.run("CALL db.relationshipTypes").stream().map(
+                        l -> l.get(0).asString()).toArray(String[]::new);
+                final var writer = new PrintWriter(stream, true, StandardCharsets.UTF_8);
+                writer.println("{");
+                writer.println("  \"graph\": {");
+                writer.println("    \"nodes\": {");
+                boolean first = true;
+                {
+                    final var result = session.run("MATCH (n) RETURN n");
+                    while (result.hasNext()) {
+                        final var record = result.next();
+                        final var node = record.get(0).asNode();
+                        if (!first)
+                            writer.println(",");
+                        first = false;
+                        writer.println("      \"" + node.elementId() + "\": {");
+                        writer.println("        \"label\": \"" + String.join(";", node.labels()) + "\",");
+                        writer.println("        \"metadata\": " + createMetadata(node));
+                        writer.print("      }");
+                    }
+                }
+                if (!first)
+                    writer.println();
+                writer.println("    },");
+                writer.println("    \"edges\": [");
+                first = true;
+                for (final String label : relationshipTypes) {
+                    final var result = session.run("MATCH ()-[r:" + label + "]->() RETURN r");
+                    while (result.hasNext()) {
+                        final var record = result.next();
+                        final var edge = record.get(0).asRelationship();
+                        if (!first)
+                            writer.println(",");
+                        first = false;
+                        writer.println("      {");
+                        writer.println("        \"id\": \"" + edge.elementId() + "\",");
+                        writer.println("        \"source\": \"" + edge.startNodeElementId() + "\",");
+                        writer.println("        \"target\": \"" + edge.endNodeElementId() + "\",");
+                        writer.println("        \"label\": \"" + label + "\",");
+                        writer.println("        \"metadata\": " + createMetadata(edge));
+                        writer.print("      }");
+                    }
+                }
+                if (!first)
+                    writer.println();
+                writer.println("    ]");
+                writer.println("  }");
+                writer.println("}");
             }
+        } catch (IOException ex) {
+            LOGGER.error("Failed to export", ex);
         }
     }
 
     private AuthToken getAuthToken(final String username, final String password) {
         return StringUtils.isEmpty(username) ? AuthTokens.none() : AuthTokens.basic(username, password);
+    }
+
+    private String createMetadata(final Entity entity) throws JsonProcessingException {
+        final Map<String, Object> properties = new HashMap<>();
+        for (final var key : entity.keys()) {
+            final var value = entity.get(key);
+            properties.put(key, value.asObject());
+        }
+        return MAPPER.writeValueAsString(properties);
     }
 }
